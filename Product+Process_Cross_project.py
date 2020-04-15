902b1fcadc99fd9ab752252e7e7c972400c8a4a5
Suvodeep Majumder <suvodeep.majumder90@gmail.com>
2020-04-15 16:02:52 -0400

    threaing added

--- Product+Process_Cross_project.py
+++ Product+Process_Cross_project.py
@@ -15,8 +15,6 @@ from scipy.spatial.distance import pdist
 from scipy.linalg import cholesky
 from scipy.io import loadmat
 
-import matlab.engine as engi
-import matlab as mat
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
@@ -27,11 +25,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.decomposition import PCA
-from pyearth import Earth
+# from pyearth import Earth
 
 from src import SMOTE
 from src import CFS
 from src import metrices_V2 as metrices
+
+from multiprocessing import Pool, cpu_count
+from threading import Thread
+from multiprocessing import Queue
 
 import platform
 from os import listdir
@@ -49,6 +51,20 @@ import matplotlib.pyplot as plt
 
 # In[ ]:
 
+
+class ThreadWithReturnValue(Thread):
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs={}, Verbose=None):
+        Thread.__init__(self, group, target, name, args, kwargs)
+        self._return = None
+    def run(self):
+        #print(type(self._target))
+        if self._target is not None:
+            self._return = self._target(*self._args,
+                                                **self._kwargs)
+    def join(self, *args):
+        Thread.join(self, *args)
+        return self._return
 
 def load_data(project):
     understand_path = 'data/understand_files_all/' + project + '_understand.csv'
@@ -190,59 +206,83 @@ projects = proj_df.repo_name.tolist()
 # In[ ]:
 
 
-precision_list = {}
-recall_list = {}
-pf_list = {}
-f1_list = {}
-g_list = {}
-auc_list = {}
-pci_20_list = {}
-ifa_list = {}
-featue_importance = {}
-for project in projects:
-    try:
-        if project == '.DS_Store':
+def run(projects,all_projects):
+    precision_list = {}
+    recall_list = {}
+    pf_list = {}
+    f1_list = {}
+    g_list = {}
+    auc_list = {}
+    pci_20_list = {}
+    ifa_list = {}
+    featue_importance = {}
+    for project in projects[0:10]:
+        try:
+            if project == '.DS_Store':
+                continue
+    #         if project != 'org.alloytools.alloy':
+    #             continue
+    #         if project != 'guice':
+    #             continue
+            print("+++++++++++++++++   "  + project + "  +++++++++++++++++")
+            recall,precision,pf,f1,g_score,auc,pci_20,ifa,importance = run_self(project,all_projects)
+#             print(recall,precision,pf,f1,g_score,auc,pci_20,ifa)
+            recall_list[project] = recall
+            precision_list[project] = precision
+            pf_list[project] = pf
+            f1_list[project] = f1
+            g_list[project] = g_score
+            auc_list[project] = auc
+            pci_20_list[project] = pci_20
+            ifa_list[project] = ifa
+            featue_importance[project] = importance
+        except Exception as e:
+            print(e)
             continue
-#         if project != 'org.alloytools.alloy':
-#             continue
-#         if project != 'guice':
-#             continue
-        print("+++++++++++++++++   "  + project + "  +++++++++++++++++")
-        recall,precision,pf,f1,g_score,auc,pci_20,ifa,importance = run_self(project,projects)
-        print(recall,precision,pf,f1,g_score,auc,pci_20,ifa)
-        recall_list[project] = recall
-        precision_list[project] = precision
-        pf_list[project] = pf
-        f1_list[project] = f1
-        g_list[project] = g_score
-        auc_list[project] = auc
-        pci_20_list[project] = pci_20
-        ifa_list[project] = ifa
-        featue_importance[project] = importance
-    except Exception as e:
-        print(e)
-        continue
-final_result = {}
-final_result['precision'] = precision_list
-final_result['recall'] = recall_list
-final_result['pf'] = pf_list
-final_result['f1'] = f1_list
-final_result['g'] = g_list
-final_result['auc'] = auc_list
-final_result['pci_20'] = pci_20_list
-final_result['ifa'] = ifa_list
+    final_result = {}
+    final_result['precision'] = precision_list
+    final_result['recall'] = recall_list
+    final_result['pf'] = pf_list
+    final_result['f1'] = f1_list
+    final_result['g'] = g_list
+    final_result['auc'] = auc_list
+    final_result['pci_20'] = pci_20_list
+    final_result['ifa'] = ifa_list
+    return final_result
+
+
+
+projects = projects[0:10]
+threads = []
+results = {}
+results['precision'] = {}
+results['recall'] = {}
+results['pf'] = {}
+results['f1'] = {}
+results['g'] = {}
+results['auc'] = {}
+results['pci_20'] = {}
+results['ifa'] = {}
+cores = cpu_count()
+split_projects = np.array_split(projects, cores)
+for i in range(cores):
+    print("starting thread ",i)
+    t = ThreadWithReturnValue(target = run, args = [split_projects[i],projects])
+    threads.append(t)
+for th in threads:
+    th.start()
+for th in threads:
+    response = th.join()
+    print(response)
+    results['precision'].update(response['precision'])
+    results['recall'].update(response['recall'])
+    results['pf'].update(response['pf'])
+    results['f1'].update(response['f1'])
+    results['g'].update(response['g'])
+    results['auc'].update(response['auc'])
+    results['pci_20'].update(response['pci_20'])
+    results['ifa'].update(response['ifa'])
+
+
 with open('results/Performance/process+product_cross_project.pkl', 'wb') as handle:
-    pickle.dump(final_result, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+    pickle.dump(results, handle, protocol=pickle.HIGHEST_PROTOCOL)
